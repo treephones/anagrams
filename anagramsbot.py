a3f1053d1eeea959f9d98ac30014384d25f29316
Moez B <moezbajwa@hotmail.com>
2020-11-07 02:58:42 -0500

    data trasfer + coords

--- anagramsbot.py
+++ anagramsbot.py
@@ -17,14 +17,18 @@ def get_all_words(string):
         3: []
     }
 
+    def are_reps(inds):
+        return len(set(inds)) != len(inds)
+
     def get_threes(string): #works because string is never larger than 6
         ret = []
         for i in range(len(string)):
             for j in range(len(string)):
                 for k in range(len(string)):
-                    comb = string[i] + string[j] + string[k]
-                    if comb not in ret:
-                        ret.append(comb)
+                    if not are_reps([i,j,k]):
+                        comb = string[i] + string[j] + string[k]
+                        if comb not in ret:
+                            ret.append(comb)
         return ret
 
 #remove repeating letters from get_fours and get_threes
@@ -34,9 +38,10 @@ def get_all_words(string):
             for j in range(len(string)):
                 for k in range(len(string)):
                     for g in range(len(string)):
-                        comb = string[i] + string[j] + string[k] + string[g]
-                        if comb not in ret:
-                            ret.append(comb)
+                        if not are_reps([i,j,k,g]):
+                            comb = string[i] + string[j] + string[k] + string[g]
+                            if comb not in ret:
+                                ret.append(comb)
         return ret
 
     def get_fives(string):
@@ -55,16 +60,18 @@ def get_all_words(string):
     possibs += get_fives(string)
     possibs += get_fours(string)
     possibs += get_threes(string)
-
+    r = []
     for p in possibs:
         try:
             if dict[p] == 1:
                 dict[p] = 2  # already used
                 words[len(p)].append(p)
+                r.append(p)
         except KeyError:
             # not a word
             continue
-    return words
+    return r
+
 
 capture = Screenshot("LonelyScreen AirPlay Receiver")
 print("Press 's' when you are on the screen with the letters visible:")
@@ -83,27 +90,28 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Moez\AppData\Local\Tesseract-
 
 config = r'--oem 3 --psm 6'
 letters = "".join(list(pytesseract.image_to_string(frame, config=config))[:-2]).lower()
+print(letters)
 all_words = get_all_words(letters)
 coords = {
-    letters[0]: (1,1),
-    letters[1]: (1,1),
-    letters[2]: (1,1),
-    letters[3]: (1,1),
-    letters[4]: (1,1),
-    letters[5]: (1,1),
-    'enter': (1,1)
+    letters[0]: 0,
+    letters[1]: 32,
+    letters[2]: 64,
+    letters[3]: 96,
+    letters[4]: 128,
+    letters[5]: 160
 }
 s = serial.Serial(
-    port="",
-    baudrate=38400
+    port='COM5',
+    baudrate=9600
 )
 
+curr = 0
 for word in all_words:
     for letter in word:
-        s.write(bytearray(coords[letter]))
-        sleep(0.2)
-    s.write(bytearray(coords['enter']))
-    
+        s.write(str(coords[letter] - curr).encode())
+        curr = coords[letter]
+    s.write('1'.encode())
+
 #show image after done
 while True:
     cv2.imshow("Processed", frame)
